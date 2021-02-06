6748c8d52053982d3605b6f97fc6057065fc2447
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-06 20:44:22 +0530

    connect to mongo

--- server/app.js
+++ server/app.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const { MONGOURI } = require("./keys");
+
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send('Hello world');
+mongoose.connect(MONGOURI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
+
+mongoose.connection.on("connected", () => {
+  console.log("yay! connected to mongo");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("error connecting to mongo",err);
+});
+// const customMiddleware = (req, res, next) => {
+//   console.log("middleware executed");
+//   next();
+// };
+
+// app.get("/", (req, res) => {
+//   console.log("home");
+//   res.send("Hello world");
+// });
+
+// app.get("/about", customMiddleware, (req, res) => {
+//   console.log("about page");
+//   res.send("about page");
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port : ${PORT}`);
---
+++ server/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGOURI:
+    "mongodb+srv://ridsuteri:tkX3aKvnXQqPRADo@cluster0.r3iji.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
