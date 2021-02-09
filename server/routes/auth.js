c147ad990a3875abc574e44787b9bdeb7d05294d
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-09 19:49:32 +0530

    hash password and add user login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const User = mongoose.model("User");
-
+const bcrypt = require("bcryptjs");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -22,24 +22,50 @@ router.post("/signup", (req, res) => {
         return res.status(422).json({ error: "Email already in use" });
       }
 
-      const user = new User({
-        email,
-        name,
-        password,
-      });
-
-      user
-        .save()
-        .then((user) => {
-          res.json({ message: "Signup successfull" });
-        })
-        .catch((err) => {
-          console.log(err);
+      bcrypt.hash(password, 12).then((hashedpassword) => {
+        const user = new User({
+          email,
+          name,
+          password: hashedpassword,
         });
+
+        user
+          .save()
+          .then((user) => {
+            res.json({ message: "Signup successfull" });
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(422).json({ error: "Please fill all details" });
+  }
+
+  User.findOne({ email: email }).then((savedUser) => {
+    if (!savedUser) {
+      return res.status(422).json({ error: "User doesn't exists " });
+    }
+
+    bcrypt
+      .compare(password, savedUser.password)
+      .then((doMatch) => {
+        if (doMatch) return res.json({ message: "Sign in successfull" });
+        else
+          return res.status(422).json({ error: "Invalid email or password" });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
 });
 
 module.exports = router;
