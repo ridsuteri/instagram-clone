df0cc85f86d1d9d31e95122e203b2b4ce150439e
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-12 10:10:16 +0530

    create react project and add react route

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./components/screens/Home";
+import SignUp from "./components/screens/Signup";
+import Profile from "./components/screens/Profile";
+import Signin from "./components/screens/Signin";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Route exact path="/">
+        <Home />
+      </Route>
+
+      <Route path="/signin">
+        <Signin />
+      </Route>
+
+      <Route path="/signup">
+        <SignUp />
+      </Route>
+
+      <Route path="/profile">
+        <Profile />
+      </Route>
+    </BrowserRouter>
+  );
+}
+
+export default App;
