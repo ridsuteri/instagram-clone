5e5a8db36481ec6c09a997d25de56ecf02599df5
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-12 12:01:32 +0530

    create signin and signup ui

--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,7 +1,21 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Signin = () => {
-  return <h1>Signin</h1>;
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className="btn waves-effect #81d4fa light-blue lighten-3">
+          Signin
+        </button>
+        <h6>
+          <Link to="/signup">Don't have an account ?</Link>
+        </h6>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- client/src/components/screens/Signup.js
+++ client/src/components/screens/Signup.js
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Signup = () => {
-  return <h1>Signup</h1>;
+  return (
+    <div className="mycard">
+      <div className="card auth-card input-field">
+        <h2>Instagram</h2>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Email" />
+        <input type="password" placeholder="Password" />
+        <button className="btn waves-effect #81d4fa light-blue lighten-3">
+          Signup
+        </button>
+        <h6>
+          <Link to="/signin">Already have an account ?</Link>
+        </h6>
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
