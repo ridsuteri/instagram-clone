793b539d56b1db069fc134b2b73ce6e18f2d4cdb
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-14 00:25:10 +0530

    create home page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -1,7 +1,70 @@
 import React from "react";
 
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <div className="home">
+      <div className="card home-card">
+        <h5>Random</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGxwYXBlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </div>
+
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title here</h6>
+          <p>This is first post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Random</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGxwYXBlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </div>
+
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title here</h6>
+          <p>This is first post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Random</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGxwYXBlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </div>
+
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title here</h6>
+          <p>This is first post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+      <div className="card home-card">
+        <h5>Random</h5>
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1485470733090-0aae1788d5af?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTJ8fHdhbGxwYXBlcnxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" />
+        </div>
+
+        <div className="card-content">
+          <i class="material-icons" style={{ color: "red" }}>
+            favorite
+          </i>
+          <h6>title here</h6>
+          <p>This is first post</p>
+          <input type="text" placeholder="add a comment" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
