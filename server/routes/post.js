49de0dce9f6de9dbd2476b6254a5c6f64169d8b6
Riddhi Suteri <ridsuteri@gmail.com>
2021-02-22 00:14:24 +0530

    implement comments

--- server/routes/post.js
+++ server/routes/post.js
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get("/allpost", requireLogin, (req, res) => {
   Post.find()
     .populate("postedBy", "_id name")
+    .populate("comments.postedBy", "_id name")
     .then((posts) => {
       res.json({ posts });
     })
@@ -85,6 +86,31 @@ router.put("/unlike", requireLogin, (req, res) => {
       res.json(result);
     }
   });
+});
+
+router.put("/comment", requireLogin, (req, res) => {
+  const comment = {
+    text: req.body.text,
+    postedBy: req.user._id,
+  };
+  Post.findByIdAndUpdate(
+    req.body.postId,
+    {
+      $push: { comments: comment },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("comments.postedBy", "_id name")
+    .populate("postedBy", "_id name")
+    .exec((err, result) => {
+      if (err) {
+        return res.status(422).json({ error: err });
+      } else {
+        res.json(result);
+      }
+    });
 });
 
 module.exports = router;
